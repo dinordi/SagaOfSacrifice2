29223812856a9cf70b7cd5a708dee2f14342f09a
Joey van Noort <joey@lvannoort.com>
2025-03-03 17:29:45 +0100

    Implement joystick event reading and error handling in dualsense.cpp

--- SOS/src/dualsense.cpp
+++ SOS/src/dualsense.cpp
@@ -12,12 +12,33 @@ int main() {
         return 1;
     }
 
+    int fd = open("/dev/input/event0", O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening event device");
+        return 1;
+    }
+
     // Variables for joystick event
     struct js_event js_event;
+
+    struct input_event ev;
 
     // Read joystick events in a loop
     while (true) {
         // Read a joystick event
+
+        if (read(fd, &ev, sizeof(struct input_event)) > 0) 
+        {
+            if(ev.type == EV_KEY) {
+                std::cout << "Key " << (int)ev.code
+                          << (ev.value ? " pressed" : " released") << std::endl;
+            }
+            else if(ev.type == EV_ABS) {
+                std::cout << "Axis " << (int)ev.code
+                          << " value: " << ev.value << std::endl;
+            }
+        }
+
         if (read(js, &js_event, sizeof(struct js_event)) > 0) {
             // Process button events
             if (js_event.type == JS_EVENT_BUTTON) {
