07acb6667797dcefdfbed6bcbac6594363fe31f5
dinordi <joeyvannoort@icloud.com>
2025-03-13 13:46:09 +0100

    Refactor game class and main function

---
+++ SOS/include/Vec2.h
@@ -0,0 +1,47 @@
+// Vec2.h
+
+#ifndef VEC2_H
+#define VEC2_H
+
+#include <cmath>
+
+class Vec2 {
+public:
+    float x, y;
+
+    Vec2() : x(0), y(0) {}
+    Vec2(float x, float y) : x(x), y(y) {}
+
+    Vec2 operator+(const Vec2& other) const {
+        return Vec2(x + other.x, y + other.y);
+    }
+
+    Vec2 operator-(const Vec2& other) const {
+        return Vec2(x - other.x, y - other.y);
+    }
+
+    Vec2 operator*(float scalar) const {
+        return Vec2(x * scalar, y * scalar);
+    }
+
+    Vec2 operator+=(const Vec2& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    float dot(const Vec2& other) const {
+        return x * other.x + y * other.y;
+    }
+
+    float length() const {
+        return std::sqrt(x * x + y * y);
+    }
+
+    Vec2 normalize() const {
+        float len = length();
+        return Vec2(x / len, y / len);
+    }
+};
+
+#endif // VEC2_H
--- SOS/main.cpp
+++ SOS/main.cpp
@@ -1,8 +1,42 @@
-#include <iostream>
+// SOS/main.cpp
 
-int main()
-{
-	std::cout << "I love grid-game!!!" << std::endl;
+#include "engine.h"
+#include "game.h"
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+float FPS = 60.0f;
+
+int main() {
+	PhysicsEngine physicsEngine;
+	Game game;
+	std::cout << "Starting game Saga Of Sacrifice 2..." << std::endl;
+	// Add game objects to the physics engine
+	for (Object* object : game.getObjects()) {
+		physicsEngine.addObject(object);
+	}
+	std::cout << "Added game objects to the physics engine." << std::endl;
+
+	auto lastTime = std::chrono::high_resolution_clock::now();
+	auto lastRenderTime = lastTime;
+
+	std::cout << "Entering gameloop..." << std::endl;
+	while (game.isRunning()) {
+		auto currentTime = std::chrono::high_resolution_clock::now();
+		std::chrono::duration<float> elapsedTime = currentTime - lastTime;
+		float deltaTime = elapsedTime.count();
+		lastTime = currentTime;
+
+		physicsEngine.update(deltaTime);
+		game.update(deltaTime);
+		std::chrono::duration<float> renderElapsedTime = currentTime - lastRenderTime;
+		if (renderElapsedTime.count() > 1.0f / FPS)
+		{
+			game.render();
+		}
+		
+	}
 
 	return 0;
 }
