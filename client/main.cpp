940ff991f39bc42580ca0d76585c111dc7cadcce
schoolheady <dj.dorant@student.avans.nl>
2025-05-27 19:23:52 +0200

    The audiomanager instance readded

--- client/main.cpp
+++ client/main.cpp
@@ -138,17 +138,18 @@ int main(int argc, char *argv[]) {
     std::string basePathSOS = "/home/root/SagaOfSacrifice2";
     if(audio.initialize(basePathSOS) == false) {
     std::string basePathSOS = "/home/root/SagaOfSacrifice2/SOS/assets/";
+        std::cout << "AudioManager initialized successfully." << std::endl;
+        audio.loadMusic("music/menu/menu.wav");
+        audio.loadSound("sfx/001.wav");
+        audio.loadSound("sfx/jump.wav");
+        audio.playMusic();
+        audio.playSound("001");
+        audio.playSound("jump");
     if(!audio.initialize(basePathSOS)) {
         std::cerr << "Failed to initialize AudioManager." << std::endl;
     }
-    std::cout << "AudioManager initialized successfully." << std::endl;
-    audio.loadMusic("music/menu/menu.wav");
-    audio.loadSound("sfx/001.wav");
-    audio.loadSound("sfx/jump.wav");
-    audio.playMusic();
-    audio.playSound("001");
-    audio.playSound("jump");
 
+    
     if(!devMode)
     {
         Renderer renderer(path_sprites + imageName);
